2f32343f8a081c913c09d9594ed65cc08c815f45
dondanhill <dondanhill@googlemail.com>
2022-12-02 21:58:58 +0000

    fix 2015/1/b

--- 2015/1/b.ts
+++ 2015/1/b.ts
@@ -3,12 +3,12 @@ let floor = 0;
 let index = 0;
 while (index <= input.length) {
   floor += input[index] === "(" ? 1 : -1;
-  console.log(floor);
 
   if (floor === -1) {
-    console.log(index);
+    console.log(index + 1);
     break;
   }
   index++;
 }
-console.log(index - 1);
+
+Deno.exit();
