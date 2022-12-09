c4380ee31930209bd4d598f8d5629ce77a0d6eb2
dondanhill <dondanhill@googlemail.com>
2022-12-09 14:23:40 +0000

    day 8

--- 8/b.ts
+++ 8/b.ts
@@ -6,38 +6,48 @@ const width = data[0].length;
 const height = data.length;
 
 function checkNeighbours(r: number, c: number) {
-  return checkVertical(r, c) || checkHorizontal(r, c);
+  return checkVertical(r, c) * checkHorizontal(r, c);
 }
 function checkVertical(r: number, c: number) {
-  return checkUp(r, c) || checkDown(r, c);
+  return checkUp(r, c) * checkDown(r, c);
 }
 function checkHorizontal(r: number, c: number) {
-  return checkLeft(r, c) || checkRight(r, c);
+  return checkLeft(r, c) * checkRight(r, c);
 }
 function checkLeft(r: number, c: number) {
-  for (let x = 0; x < c; x++) {
-    if (data[r][x] >= data[r][c]) return false;
+  let count = 0;
+  for (let x = c - 1; x >= 0; x--) {
+    count++;
+    if (data[r][x] >= data[r][c]) return count;
   }
-  return true;
+  return count;
 }
 function checkRight(r: number, c: number) {
+  let count = 0;
   for (let x = c + 1; x < width; x++) {
-    if (data[r][x] >= data[r][c]) return false;
+    count++;
+    if (data[r][x] >= data[r][c]) return count;
   }
-  return true;
+  return count;
 }
 function checkUp(r: number, c: number) {
-  for (let x = 0; x < r; x++) {
-    if (data[x][c] >= data[r][c]) return false;
+  let count = 0;
+  for (let x = r - 1; x >= 0; x--) {
+    count++;
+    if (data[x][c] >= data[r][c]) return count;
   }
-  return true;
+  return count;
 }
 function checkDown(r: number, c: number) {
+  let count = 0;
   for (let x = r + 1; x < height; x++) {
-    if (data[x][c] >= data[r][c]) return false;
+    count++;
+    if (data[x][c] >= data[r][c]) return count;
   }
-  return true;
+  return count;
 }
+
+console.log(data);
 console.log(
   data
     .map((row, rowindex) => {
@@ -51,11 +61,11 @@ console.log(
           )
             return 1;
 
-          return checkNeighbours(rowindex, colindex) ? 1 : (0 as number);
+          return checkNeighbours(rowindex, colindex);
         })
-        .reduce((acc, value) => acc + value, 0);
+        .reduce((prev, curr) => Math.max(prev, curr), 0);
     })
-    .reduce((acc, value) => acc + value, 0)
+    .reduce((prev, curr) => Math.max(prev, curr), 0)
 );
 
 Deno.exit();
