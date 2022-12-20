abc3f66dd6a38ae5209e4ab41cc8db66268fb994
dondanhill <dondanhill@googlemail.com>
2022-12-20 09:50:46 +0000

    day 05

--- 05/a.ts
+++ 05/a.ts
@@ -2,14 +2,17 @@
 const input = await Deno.readTextFile("./input.txt");
 const [stackData, movesData] = input.split("\n\n");
 
-const stacks = stackData
-  .split("\n")
-  .flatMap((crates) => [crates.match(/.{1,4}/g)] ?? [])
-  .reduce(
-    (crates, row) => row.map((_, i) => [...(crates[i] ?? []), row[i]]),
-    []
-  )
-  ?.map((stack) => stack.reverse().filter((stack) => stack.trim() !== ""));
+const stacks =
+  stackData
+    .split("\n")
+    .map((crates) => Array.from(crates.match(/.{1,4}/g) || []))
+    .reduce(
+      (crates, row) => row.map((_, i) => [...(crates[i] ?? []), row[i]]),
+      []
+    )
+    ?.map((stack) =>
+      (stack || []).reverse().filter((stack: string) => stack.trim() !== "")
+    ) || [];
 
 const moves = movesData.split("\n").map((_) => {
   const move = _.split(" ");
